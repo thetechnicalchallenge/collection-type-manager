39a4cedefd209489de1b48a026bb860dec8c073e
thetechnicalchallenge <jean.marcel.michelet>
2021-01-09 02:39:38 +0100

    first version README.md

--- README.md
+++ README.md
@@ -1,3 +1,78 @@
-# collection-type-manager
+# collection-type-manager (Bêta version)
 
 Javascript library to easily interact with [Symfony CollectionType Field](https://symfony.com/doc/current/reference/forms/types/collection.html#adding-and-removing-items) 
+
+## Installation
+```bash
+npm i collection-type-manager
+```
+```js
+import CollectionTypeManager from "collection-type-manager";
+```
+
+## In your Symfony FormType
+First, set `allow_add` and `allow_delete` to true.
+```php
+$builder  
+  ->add('exampleCollection', CollectionType::class, [  
+  'entry_type' => ExampleCollection::class,  
+  'allow_add' => true,  
+  'allow_delete' => true,  
+  ])  
+;
+```
+
+## JavaScript
+Here is what the minimum configuration looks like:
+```js
+const ExampleCollection = new CollectionTypeManager({  
+    containerId: 'collection-fields-list', // the container id of your collection
+    addButtonId: 'add-collection-widget', // the button id for adding a widget
+    removeButtonsClassName: 'remove-collection-widget', // the class of all the remove buttons
+});
+```
+You can also configure callbacks for three events:
+```js
+let eventConfig = {
+    isBuilt: () => yourAfterBuiltFunction,  
+    afterAddElement: () => {  
+        // Here you can access to the new widget just added
+        ExampleCollection.getLastWidgetAdded();
+    },  
+    afterRemoveElement: () => yourAfterRemoveFunction
+}
+```
+
+## Form theme
+The easiest way to create your design is to create a [form theme](https://symfony.com/doc/current/form/form_themes.html). For this example, I propose you to implement a theme in [the same template as your form](https://symfony.com/doc/current/form/form_themes.html#creating-a-form-theme-in-the-same-template-as-the-form). 
+
+Here is a very basic example that you can customize:
+*As you can see, the name of your field must begin with an underscore "_" and be converted to [snake case](https://en.wikipedia.org/wiki/Snake_case) format*.
+```twig-template
+{% block _example_collection_widget %}  
+{# the container of widgets #}
+<ul id="collection-fields-list"  
+      data-prototype="{{ form_widget(form.vars.prototype)|e }}"  
+      data-counter="{{ form|length }}">  
+     {% for collection in form %}  
+            {{ form_widget(collection) }}  
+     {% endfor %}  
+</ul>  
+  
+ {# data-target indicates the container id where add the new widget #}
+ <button data-target="#collection-fields-list" id="add-collection-widget" type="button">Add</button>  
+{% endblock %}  
+  
+{% block _example_collection_entry_widget %}  
+{# A widget in the loop #}
+ <div id="{{id}}">  
+  {{form_row(form.answer)}} 
+ {# 
+    data-target indicates the widget id to remove
+    don't forget the class for the remove button
+  #}
+  <button type="button" data-target="{{id}}" class="remove-collection-widget"> Remove</button>  
+ </div>
+{% endblock %}
+```
+*PR and constructive criticism are welcome :)*
--- src/CollectionTypeManager.js
+++ src/CollectionTypeManager.js
@@ -1,73 +1,90 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
 /**
  * @author thetechnicalchallenge@gmail.com
  */
-export class CollectionTypeManager {
-    constructor(settings = {
-        containerId: null,
-        addButtonId: null,
-        removeButtonsClassName: null,
-        isBuilt: null,
-        afterAddElement: null,
-        afterRemoveElement: null
-    }) {
-        this.settings = settings;
-        this.container = document.getElementById(settings.containerId);
-        this.addButton = document.getElementById(settings.addButtonId);
-        this.removeButtons = Array.from(document.getElementsByClassName(settings.removeButtonsClassName))
+var CollectionTypeManager = /*#__PURE__*/function () {
+  function CollectionTypeManager() {
+    var settings = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {
+      containerId: null,
+      addButtonId: null,
+      removeButtonsClassName: null,
+      isBuilt: null,
+      afterAddElement: null,
+      afterRemoveElement: null
+    };
 
-        this.widgetPrototype = this.container.dataset.prototype;
-        this.counter = this.container.dataset.counter;
+    _classCallCheck(this, CollectionTypeManager);
 
-        this.lastWidgetAdded = null;
+    this.settings = settings;
+    this.container = document.getElementById(settings.containerId);
+    this.addButton = document.getElementById(settings.addButtonId);
+    this.removeButtons = Array.from(document.getElementsByClassName(settings.removeButtonsClassName));
+    this.widgetPrototype = this.container.dataset.prototype;
+    this.counter = this.container.dataset.counter;
+    this.lastWidgetAdded = null;
+    this.listenButtons();
 
-        this.listenButtons();
-
-        if (settings.isBuilt instanceof Function) {
-            settings.isBuilt();
-        }
+    if (settings.isBuilt instanceof Function) {
+      settings.isBuilt();
     }
+  }
 
-    listenButtons()
-    {
-        this.addButton.addEventListener('click', this.addElement.bind(this));
+  _createClass(CollectionTypeManager, [{
+    key: "listenButtons",
+    value: function listenButtons() {
+      var _this = this;
 
-        this.removeButtons.forEach((removeButton, index) => {
-            removeButton.addEventListener('click', this.removeElement.bind(this, removeButton));
-        });
+      this.addButton.addEventListener('click', this.addElement.bind(this));
+      this.removeButtons.forEach(function (removeButton, index) {
+        removeButton.addEventListener('click', _this.removeElement.bind(_this, removeButton));
+      });
     }
+  }, {
+    key: "addElement",
+    value: function addElement() {
+      var widgetContainer = document.createElement('div');
+      widgetContainer.innerHTML = this.widgetPrototype.replace(/__name__/g, this.counter);
+      var removeButton = widgetContainer.children[0].getElementsByClassName(this.settings.removeButtonsClassName)[0];
+      removeButton.addEventListener('click', this.removeElement.bind(this, removeButton));
+      this.lastWidgetAdded = widgetContainer.children[0];
+      this.container.appendChild(widgetContainer.children[0]);
+      this.counter++;
 
-    addElement()
-    {
-        let widgetContainer = document.createElement('div');
-        widgetContainer.innerHTML = this.widgetPrototype.replace(/__name__/g, this.counter);
-
-        let removeButton = widgetContainer.children[0].getElementsByClassName(this.settings.removeButtonsClassName)[0];
-        removeButton.addEventListener('click', this.removeElement.bind(this, removeButton));
-
-        this.lastWidgetAdded = widgetContainer.children[0];
-
-        this.container.appendChild(widgetContainer.children[0]);
-        this.counter++;
-
-        if (this.settings.afterAddElement instanceof Function) {
-            this.settings.afterAddElement();
-        }
+      if (this.settings.afterAddElement instanceof Function) {
+        this.settings.afterAddElement();
+      }
     }
-
-    getLastWidgetAdded()
-    {
-        return this.lastWidgetAdded;
+  }, {
+    key: "getLastWidgetAdded",
+    value: function getLastWidgetAdded() {
+      return this.lastWidgetAdded;
     }
+  }, {
+    key: "removeElement",
+    value: function removeElement(removeButton) {
+      var targetElement = document.getElementById(removeButton.dataset.target);
+      removeButton.removeEventListener('click', this.removeElement.bind(this, removeButton));
+      this.container.removeChild(targetElement);
 
-    removeElement(removeButton)
-    {
-        let targetElement = document.getElementById(removeButton.dataset.target);
-        removeButton.removeEventListener('click', this.removeElement.bind(this, removeButton));
-
-        this.container.removeChild(targetElement);
-
-        if (this.settings.afterRemoveElement instanceof Function) {
-            this.settings.afterRemoveElement();
-        }
+      if (this.settings.afterRemoveElement instanceof Function) {
+        this.settings.afterRemoveElement();
+      }
     }
-}
+  }]);
+
+  return CollectionTypeManager;
+}();
+
+exports.default = CollectionTypeManager;
